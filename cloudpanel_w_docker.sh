3ac6ca126049cdd41bede6ce759dcf5c74046d4c
Narodko Artem Romanovich <zer0cool4ek@gmail.com>
2023-07-24 13:43:58 +0300

    Update cloudpanel_w_docker.sh

--- cloudpanel_w_docker.sh
+++ cloudpanel_w_docker.sh
@@ -2,24 +2,45 @@
 
 # wget -qO- https://raw.githubusercontent.com/Zer0CooL4ek/etc/main/cloudpanel_w_docker.sh | bash
 
-# Update package list and upgrade installed packages
-echo "Updating package list and upgrading installed packages"
-apt-get update && sudo apt-get upgrade -y && sudo apt-get autoremove -y
+# Обновление списка пакетов и обновление установленных пакетов
+echo "Обновление списка пакетов и обновление установленных пакетов"
+apt-get update > /dev/null && sudo apt-get upgrade -y > /dev/null && sudo apt-get autoremove -y > /dev/null
 
-# Install sudo and curl packages
-echo "Installing sudo and curl packages"
-apt-get install sudo curl -y
+# Установка пакетов sudo и curl
+echo "Установка пакетов sudo и curl"
+apt-get install sudo curl -y > /dev/null
 
-# Modify SSH configuration file to disable password authentication and restart SSH service
-echo "Modifying SSH configuration file to disable password authentication"
+# Изменение файла конфигурации SSH для отключения аутентификации по паролю и перезапуск службы SSH
+echo "Изменение файла конфигурации SSH для отключения аутентификации по паролю"
 if grep -q "^PasswordAuthentication yes" /etc/ssh/sshd_config; then
     sed -i 's/PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
-    echo "PasswordAuthentication has been changed to no"
+    echo "PasswordAuthentication изменено на no"
 else
-    echo "PasswordAuthentication is already set to no"
+    echo "PasswordAuthentication уже установлено на no"
 fi
 
+# Загрузка скрипта установки Docker и его выполнение
+echo "Загрузка скрипта установки Docker и его выполнение"
+curl -fsSL https://get.docker.com -o get-docker.sh > /dev/null && sudo sh get-docker.sh > /dev/null
 
-# Add crontab jobs for daily updates of CloudPanel, system, and Docker Compose (in one command)
-echo "Adding crontab jobs for daily updates of CloudPanel, system, and Docker Compose (in one command)"
-sh -c 'if ! grep -q "clp-update" /etc/crontab; then printf "\n# Update CloudPanel every day at 4 AM\n0 4 * * * root clp-update\n\n# Update system every day at 4:10 AM\n10 4 * * * root apt-get update && apt-get upgrade -y && apt-get autoremove -y\n\n# Update Docker Compose every day at 4:30 AM\n30 4 * * * root curl -L \"https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)\" -o /usr/local/bin/docker-compose && chmod +x /usr/local/bin/docker-compose\n" >> /etc/crontab; fi'
+# Добавление текущего пользователя в группу docker
+echo "Добавление текущего пользователя в группу docker"
+usermod -aG docker $USER > /dev/null
+
+# Загрузка последней версии Docker Compose
+echo "Загрузка последней версии Docker Compose"
+curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose > /dev/null
+
+# Изменение разрешений файла Docker Compose, чтобы сделать его исполняемым
+echo "Изменение разрешений файла Docker Compose, чтобы сделать его исполняемым"
+chmod +x /usr/local/bin/docker-compose > /dev/null
+
+# Загрузка скрипта установки CloudPanel, проверка его контрольной суммы и выполнение с указанным DB_ENGINE
+echo "Загрузка скрипта установки CloudPanel, проверка его контрольной суммы и выполнение с указанным DB_ENGINE"
+curl -sS https://installer.cloudpanel.io/ce/v2/install.sh -o install.sh > /dev/null; \
+echo "3c30168958264ced81ca9b58dbc55b4d28585d9066b9da085f2b130ae91c50f6 install.sh" | \
+sha256sum -c && sudo DB_ENGINE=MARIADB_10.11 bash install.sh > /dev/null
+
+# Добавление заданий crontab для ежедневного обновления CloudPanel, системы и Docker Compose (в одной команде)
+echo "Добавление заданий crontab для ежедневного обновления CloudPanel, системы и Docker Compose (в одной команде)"
+sh -c 'if ! grep -q "clp-update" /etc/crontab; then printf "\n# Обновление CloudPanel каждый день в 4 часа утра\n0 4 * * * root clp-update\n\n# Обновление системы каждый день в 4:10 утра\n10 4 * * * root apt-get update && apt-get upgrade -y && apt-get autoremove -y\n\n# Обновление Docker Compose каждый день в 4:30 утра\n30 4 * * * root curl -L \"https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)\" -o /usr/local/bin/docker-compose && chmod +x /usr/local/bin/docker-compose\n" >> /etc/crontab; fi' > /dev/null

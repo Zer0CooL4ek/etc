803aa1a9374c1836fdaf0a7b012d2e01ba4a9ac6
Narodko Artem Romanovich <zer0cool4ek@gmail.com>
2024-10-02 13:26:58 +0300

    Update def.sh

--- def.sh
+++ def.sh
@@ -26,16 +26,16 @@ progress() {
 
 # Update package list and upgrade installed packages
 echo -e "${GREEN}Updating package list and upgrading installed packages${NC}"
-progress &
+progress & 
 PROGRESS_PID=$!
 apt update > /dev/null 2>&1 && apt upgrade -y > /dev/null 2>&1 && apt autoremove -y > /dev/null 2>&1 && echo -e "\r${CHECKMARK} Package list updated and installed packages upgraded" || { kill $PROGRESS_PID; echo -e "\r${CROSS} Error: Failed to update and upgrade packages"; exit 1; }
 kill $PROGRESS_PID
 
-# Install sudo and curl packages
-echo -e "${GREEN}Installing sudo and curl and htop packages${NC}"
+# Install sudo, curl, htop, and neofetch packages
+echo -e "${GREEN}Installing sudo, curl, htop, and neofetch packages${NC}"
 progress &
 PROGRESS_PID=$!
-apt install sudo curl htop neofetch -y > /dev/null 2>&1 && echo -e "\r${CHECKMARK} Sudo and curl packages installed" || { kill $PROGRESS_PID; echo -e "\r${CROSS} Error: Failed to install sudo and curl"; exit 1; }
+apt install sudo curl htop neofetch -y > /dev/null 2>&1 && echo -e "\r${CHECKMARK} Sudo, curl, htop, and neofetch packages installed" || { kill $PROGRESS_PID; echo -e "\r${CROSS} Error: Failed to install packages"; exit 1; }
 kill $PROGRESS_PID
 
 # Clean cache and temporary files
@@ -45,7 +45,7 @@ PROGRESS_PID=$!
 apt clean > /dev/null 2>&1 && rm -rf /tmp/* > /dev/null 2>&1 && echo -e "\r${CHECKMARK} Cache and temporary files cleaned" || { kill $PROGRESS_PID; echo -e "\r${CROSS} Error: Failed to clean cache and temporary files"; exit 1; }
 kill $PROGRESS_PID
 
-# Check current SSH port and changing it to 2224 if necessary
+# Check current SSH port and change it to 2224 if necessary
 echo -e "${GREEN}Checking current SSH port and changing it to 2224 if necessary${NC}"
 if ! grep -q "^Port 2224" /etc/ssh/sshd_config; then
     sed -i 's/^#\?Port [0-9]*/Port 2224/' /etc/ssh/sshd_config
@@ -56,75 +56,55 @@ fi
 
 # Modify SSH configuration file to disable password authentication
 echo -e "${GREEN}Modifying SSH configuration file to disable password authentication${NC}"
-if grep -q "^#\?PasswordAuthentication yes" /etc/ssh/sshd_config; then
+if grep -q "^PasswordAuthentication no" /etc/ssh/sshd_config; then
+    echo -e "\r${CHECKMARK} PasswordAuthentication is already set to no"
+else
     sed -i 's/^#\?PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
     echo -e "\r${CHECKMARK} PasswordAuthentication has been changed to no"
-else
-    echo -e "\r${CHECKMARK} PasswordAuthentication is already set to no"
 fi
 
 # Set PermitEmptyPasswords to no
 echo -e "${GREEN}Setting PermitEmptyPasswords to no${NC}"
-if grep -q "^#\?PermitEmptyPasswords" /etc/ssh/sshd_config; then
+if grep -q "^PermitEmptyPasswords no" /etc/ssh/sshd_config; then
+    echo -e "\r${CHECKMARK} PermitEmptyPasswords is already set to no"
+else
     sed -i 's/^#\?PermitEmptyPasswords .*/PermitEmptyPasswords no/' /etc/ssh/sshd_config
     echo -e "\r${CHECKMARK} PermitEmptyPasswords has been set to no"
-else
-    echo -e "\r${CHECKMARK} PermitEmptyPasswords is already set to no"
 fi
 
-# Check and set MaxAuthTries
+# Set MaxAuthTries to 3
 echo -e "${GREEN}Setting MaxAuthTries to 3${NC}"
-if grep -q "^MaxAuthTries" /etc/ssh/sshd_config; then
-    if ! grep -q "^MaxAuthTries 3" /etc/ssh/sshd_config; then
-        sed -i 's/^MaxAuthTries .*/MaxAuthTries 3/' /etc/ssh/sshd_config
-        echo -e "\r${CHECKMARK} MaxAuthTries has been set to 3"
-    else
-        echo -e "\r${CHECKMARK} MaxAuthTries is already set to 3"
-    fi
+if grep -q "^MaxAuthTries 3" /etc/ssh/sshd_config; then
+    echo -e "\r${CHECKMARK} MaxAuthTries is already set to 3"
 else
-    echo "MaxAuthTries 3" >> /etc/ssh/sshd_config
+    sed -i 's/^#\?MaxAuthTries .*/MaxAuthTries 3/' /etc/ssh/sshd_config
     echo -e "\r${CHECKMARK} MaxAuthTries has been set to 3"
 fi
 
-# Check and set MaxSessions
+# Set MaxSessions to 2
 echo -e "${GREEN}Setting MaxSessions to 2${NC}"
-if grep -q "^MaxSessions" /etc/ssh/sshd_config; then
-    if ! grep -q "^MaxSessions 2" /etc/ssh/sshd_config; then
-        sed -i 's/^MaxSessions .*/MaxSessions 2/' /etc/ssh/sshd_config
-        echo -e "\r${CHECKMARK} MaxSessions has been set to 2"
-    else
-        echo -e "\r${CHECKMARK} MaxSessions is already set to 2"
-    fi
+if grep -q "^MaxSessions 2" /etc/ssh/sshd_config; then
+    echo -e "\r${CHECKMARK} MaxSessions is already set to 2"
 else
-    echo "MaxSessions 2" >> /etc/ssh/sshd_config
+    sed -i 's/^#\?MaxSessions .*/MaxSessions 2/' /etc/ssh/sshd_config
     echo -e "\r${CHECKMARK} MaxSessions has been set to 2"
 fi
 
-# Check and set ClientAliveInterval
+# Set ClientAliveInterval to 300
 echo -e "${GREEN}Setting ClientAliveInterval to 300${NC}"
-if grep -q "^ClientAliveInterval" /etc/ssh/sshd_config; then
-    if ! grep -q "^ClientAliveInterval 300" /etc/ssh/sshd_config; then
-        sed -i 's/^ClientAliveInterval .*/ClientAliveInterval 300/' /etc/ssh/sshd_config
-        echo -e "\r${CHECKMARK} ClientAliveInterval has been set to 300"
-    else
-        echo -e "\r${CHECKMARK} ClientAliveInterval is already set to 300"
-    fi
+if grep -q "^ClientAliveInterval 300" /etc/ssh/sshd_config; then
+    echo -e "\r${CHECKMARK} ClientAliveInterval is already set to 300"
 else
-    echo "ClientAliveInterval 300" >> /etc/ssh/sshd_config
+    sed -i 's/^#\?ClientAliveInterval .*/ClientAliveInterval 300/' /etc/ssh/sshd_config
     echo -e "\r${CHECKMARK} ClientAliveInterval has been set to 300"
 fi
 
-# Check and set ClientAliveCountMax
+# Set ClientAliveCountMax to 0
 echo -e "${GREEN}Setting ClientAliveCountMax to 0${NC}"
-if grep -q "^ClientAliveCountMax" /etc/ssh/sshd_config; then
-    if ! grep -q "^ClientAliveCountMax 0" /etc/ssh/sshd_config; then
-        sed -i 's/^ClientAliveCountMax .*/ClientAliveCountMax 0/' /etc/ssh/sshd_config
-        echo -e "\r${CHECKMARK} ClientAliveCountMax has been set to 0"
-    else
-        echo -e "\r${CHECKMARK} ClientAliveCountMax is already set to 0"
-    fi
+if grep -q "^ClientAliveCountMax 0" /etc/ssh/sshd_config; then
+    echo -e "\r${CHECKMARK} ClientAliveCountMax is already set to 0"
 else
-    echo "ClientAliveCountMax 0" >> /etc/ssh/sshd_config
+    sed -i 's/^#\?ClientAliveCountMax .*/ClientAliveCountMax 0/' /etc/ssh/sshd_config
     echo -e "\r${CHECKMARK} ClientAliveCountMax has been set to 0"
 fi
 
